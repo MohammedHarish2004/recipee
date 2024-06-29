88a97c55f9ae61e695dbdf6db992a3445587723e
MohammedHarish2004 <clashingharris2004@gmail.com>
2024-06-29 16:41:41 +0530

    Create Pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
 import { Button } from 'flowbite-react'
 import React from 'react'
-
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Search from './Pages/Search'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+import Details from './Pages/Details'
 export default function App() {
   return (
-    <Button>App</Button>
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/search' element={<Search />}/>
+      <Route path='/register' element={<Register />}/>
+      <Route path='/login' element={<Login />}/>
+      <Route path='/details' element={<Details />}/>
+    </Routes>
+   </BrowserRouter>
   )
 }
